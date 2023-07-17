a2ad348c7290c88c09a699f262dfac3906f11970
Ben Lynch <bdlynch7@gmail.com>
2023-07-17 14:17:56 -0500

    delete button working

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery is set up')
+  
     // handlers here
   $("#generate-button").on('click', handleGenerate)
+  $("#append-here").on('click',"#new-div", "yellow-button", handleYellow)
+  $("#append-here").on('click', ".new-div", handleDelete)
 
 } // end on Ready
 
@@ -11,28 +14,33 @@ function onReady() {
 let generateCount = 0;
 
 
-function handleGenerate() {
+function handleGenerate(event) {
+  event.preventDefault()  
   console.log('in handleGenerate function')
   // increment count to log # of clicks
   generateCount++
   
-  $("#append-here").append(`
-    <div>
-      <p>Generate has been clicked: ${generateCount}</p>
-      <button>Yellow</button>
-      <button>Delete</button>
-    </div>
-  `)
-
+render()
  
 }
 
-function handleYellow() {
-    console.log('in handleYellow')
+function render() {
+  $("#append-here").append(`
+    <div class="new-div">
+      <p>Generate has been clicked ${generateCount} times.</p>
+      <button id="yellow-button">Yellow</button>
+      <button id="delete-button">Delete</button>
+    </div>
+`)
 
 }
 
-function handleDelete() {
-    console.log('in handleDelete')
+function handleYellow(event) {
+    event.preventDefault()
+    $(this).css("background-color", "yellow")
+}
 
+function handleDelete(event) {
+    event.preventDefault()
+    $(this).remove()
 }
